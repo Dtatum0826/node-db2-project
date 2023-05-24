609379543065c9ae9a519f8e0574f0e6ce2581bc
Dustin Tatum <dtatum0826@gmail.com>
2023-05-24 05:45:04 -0500

    Seed file prepped

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,35 @@
 // STRETCH
+const cars = [
+    {
+vin:"1111111111111",
+make:"toyota",
+model:"prius",
+mileage:135000,
+title:"clean",
+transmission:"automatic"
+    }, 
+    {
+vin:"1111111111112",
+make:"toyota",
+model:"corolla",
+mileage:155000,
+title:"salvage",
+transmission:"manual"
+    }, 
+    {
+vin:"1111111111113",
+make:"toyota",
+model:"tacoma",
+mileage:144000,
+title:"clean",
+transmission:"automatic"
+    },
+    
+]
+
+exports.sedd = async function(knex) {
+   await knex('cars').truncate()
+   await  knex('cars').insert(cars)
+    
+   
+}
