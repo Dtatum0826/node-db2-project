1f538f951ad732b3fb0c5bb2af2b5016cb4f87df
Dustin Tatum <dtatum0826@gmail.com>
2023-05-25 05:38:50 -0500

    completed MVP

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,16 +1,37 @@
 const express = require('express')
-
+const Car = require('./cars-model')
 const router = express.Router()
 
+const{
+checkCarId,
+checkCarPayload,
+checkVinNumberValid,
+checkVinNumberUnique
+} =require('./cars-middleware')
+
 router.get('/', async (req,res,next)=>{
-    res.json('gettin all cars')
+   try{
+    const cars = await Car.getAll()
+res.json(cars)
+   }catch(err){
+    next(err)
+   }
 })
-router.get('/:id', async (req,res,next)=>{
-    res.json('getting car by id')
+router.get('/:id',checkCarId, async (req,res,next)=>{
+   res.json(req.car)
+   next()
 })
-router.post('/', async (req,res,next)=>{
-    res.json('posting new car')
+router.post('/',checkCarPayload,checkVinNumberValid,checkVinNumberUnique, async (req,res,next)=>{
+try{
+const newCar = await Car.create(req.body)
+res.json(newCar)
+
+}catch(err){
+    next(err)
+}
+
 })
+
 
 
 module.exports = router
